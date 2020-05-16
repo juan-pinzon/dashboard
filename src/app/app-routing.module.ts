a362ecbf10f6fd547fe53917af4ebcb4c35486f5
Juan Carlos Pinzon <juancarlospinzon@outlook.com>
2020-05-16 16:55:56 -0500

    Integration modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: LayoutComponent,
+		children: [
+			{
+				path: '',
+				loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+			}
+		]
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LayoutComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		SharedModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
