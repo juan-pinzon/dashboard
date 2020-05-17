d401cce215ccfbd09ee1e0c430e49010e0e81ccd
Juan Carlos Pinzon <juancarlospinzon@outlook.com>
2020-05-17 06:30:09 -0500

    last details

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AsideComponent } from './components/aside/aside.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-	declarations: [AsideComponent, HeaderComponent],
+	declarations: [AsideComponent, HeaderComponent, FooterComponent],
 	imports: [
 		CommonModule
 	],
 	exports: [
 		AsideComponent,
-		HeaderComponent
+		HeaderComponent,
+		FooterComponent
 	]
 })
 export class SharedModule { }
